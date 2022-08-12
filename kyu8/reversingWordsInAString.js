bb918d7689d22bb3f6fedd6cc05bb00b2dba7b6f
pyaesonepsn <pyaesonepsn@gmail.com>
2022-08-12 20:29:21 +0630

    Create reversingWordsInAString.js

---
+++ kyu8/reversingWordsInAString.js
@@ -0,0 +1,18 @@
+/*
+You need to write a function that reverses the words in a given string.
+A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unnecessary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi"
+ */
+
+function reverse(string){
+    // change string to array
+    // reverse the array and join
+    let arr = string.split(' '); // ['Hello' 'World']
+    return arr.reverse().join(' ')
+}
