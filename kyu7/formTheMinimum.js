19870dbd77f52294b1deae624d5d0df9f87a87b5
pyaesonepsn <pyaesonepsn@gmail.com>
2022-08-15 21:07:18 +0630

    Create formTheMinimum.js

---
+++ kyu7/formTheMinimum.js
@@ -0,0 +1,9 @@
+/*
+Task
+Given a list of digits, return the smallest number that could be formed from these digits,
+using the digits only once (ignore duplicates).
+ */
+
+function minValue(values){
+    return +(Array.from(new Set(values))).sort((a,b) => a-b).join('');
+}
