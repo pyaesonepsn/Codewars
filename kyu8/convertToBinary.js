8c60c97e9040ab54bc445e117f808310a92b9ae0
pyaesonepsn <pyaesonepsn@gmail.com>
2022-08-31 22:13:34 +0630

    convertToBinary.js

---
+++ kyu8/convertToBinary.js
@@ -0,0 +1,12 @@
+/*
+Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+to_binary(1)
+to_binary(5)
+to_binary(11)
+ */
+
+function toBinary(n){
+    return Number(n.toString(2));
+}
+
