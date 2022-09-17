f05372099f842c55099f5d2840dc9ede9acbb135
pyaesonepsn <pyaesonepsn@gmail.com>
2022-09-17 23:54:30 +0630

    Update surfaceAreaVolumeOfBox.js

--- kyu8/surfaceAreaVolumeOfBox.js
+++ kyu8/surfaceAreaVolumeOfBox.js
@@ -2,3 +2,7 @@
 Write a function that returns the total surface area and volume of a box as an array: [area, volume]
  */
 
+function getSize(width, height, depth){
+    return [((width * height) + (width * depth) + (depth * height)) * 2,
+        width * height * depth]
+}
