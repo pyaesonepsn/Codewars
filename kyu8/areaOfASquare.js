800406c11bf98e388835ac2d7880af4f558afdba
pyaesonepsn <pyaesonepsn@gmail.com>
2022-10-17 18:01:10 +0630

    areaOfASquare.js

---
+++ kyu8/areaOfASquare.js
@@ -0,0 +1,7 @@
+function squareArea(A){
+    return +Math.pow((2*A/Math.PI),2).toFixed(2);
+}
+
+//circumfrence = 4*A = 2*pi*R
+//Radius = 4*A/(2*pi) = 2*A/pi
+// area of square = side of square ** 2 = Radius ** 2 = (2*A/pi) ** 2
